d08682085dc802f180cb2a59f6f3e09149f21dc4
Tahzib Mashrik <mashrikt@gmail.com>
2018-06-28 16:54:39 +0600

    set up POSTGRES database

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import dj_database_url
+
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -76,12 +78,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+DEFAULT_DATABASE = config('DATABASE_URL',
+                          default='postgres://cramstack:cramstack@localhost:5432/cramstack',
+                          cast=dj_database_url.parse)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': DEFAULT_DATABASE
 }
+
 
 
 # Password validation
