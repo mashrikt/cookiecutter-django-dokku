ea4fc703fa7c262d47cbd8379dfb321cf16a0f23
Tahzib Mashrik <mashrikt@gmail.com>
2018-07-05 04:35:16 +0600

    Set up cookiecutter template

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/wsgi.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myproject project.
+WSGI config for {{cookiecutter.app_name}} project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.app_name}}.settings")
 
 application = get_wsgi_application()
