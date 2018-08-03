3e827270b28f3bed5cbe9f2d4cbbfc87de23f872
Tahzib Mashrik <mashrikt@gmail.com>
2018-08-04 01:40:31 +0600

    pytest-cookies

---
+++ tests/test_cookiecutter_generation.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+@pytest.fixture
+def context():
+    return {
+        "github_repository_name": "myproject",
+        "app_name": "myproject",
+        "description": "My Project",
+        "email_user": "y",
+        "use_celery": "y",
+        "use_sentry": "n",
+        "use_rest_framework": "y",
+        "use_rest_auth": "y",
+        "use_restframework_documentation": "y"
+}
+
+
+def test_default_configuration(cookies, context):
+    result = cookies.bake(extra_context=context)
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project.basename == context["github_repository_name"]
+    assert result.project.isdir()
