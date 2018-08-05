5a5e1c20c59ba8413e95605f4b08d5dd87e78f28
Tahzib Mashrik <mashrikt@gmail.com>
2018-08-05 23:02:14 +0600

    handle case for failed test in test_docker file

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -15,7 +15,7 @@ cookiecutter ../../ --no-input
 cd myproject
 
 # run the project's tests
-docker-compose -f docker-compose.yml run web pytest
+docker-compose -f docker-compose.yml run web pytest || { exit 1; }
 
 # return non-zero status code if there are migrations that have not been created
 docker-compose -f docker-compose.yml run web python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; exit 1; }
